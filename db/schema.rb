711ae402fee830a85ef74b80df013bb99463be67
thuan274 <doanminhthuan274@gmail.com>
2017-08-22 16:53:45 +0700

    modify model order & order item

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814035047) do
+ActiveRecord::Schema.define(version: 20170822085401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_articles_on_product_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -26,12 +34,12 @@ ActiveRecord::Schema.define(version: 20170814035047) do
 
   create_table "order_items", force: :cascade do |t|
     t.bigint "order_id"
-    t.bigint "product_id"
-    t.integer "quality"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "article_id"
+    t.index ["article_id"], name: "index_order_items_on_article_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -79,7 +87,8 @@ ActiveRecord::Schema.define(version: 20170814035047) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "articles", "products"
+  add_foreign_key "order_items", "articles"
   add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "customers"
 end
