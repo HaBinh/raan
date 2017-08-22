ab6e18f6cfe673928f8dd696946e5479b50af6fb
thuan274 <doanminhthuan274@gmail.com>
2017-08-22 15:41:10 +0700

    add

--- db/migrate/20170814035047_create_order_items.rb
+++ db/migrate/20170814035047_create_order_items.rb
@@ -2,7 +2,6 @@ class CreateOrderItems < ActiveRecord::Migration[5.1]
   def change
     create_table :order_items do |t|
       t.references :order, foreign_key: true
-      t.references :article, foreign_key: true
       t.integer :quality
 
       t.timestamps
