ad03c18593fc2ede3ad0ab889aa0c89f6ba4b1d8
viethoc <viethocpro@gmail.com>
2017-09-11 14:30:03 +0700

    deploy

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -11,3 +11,14 @@ class StoresController < ApplicationController
         end
       end
 end
+# Code good, try it
+# @stores = Array.new
+# Product.all.each do |p|
+#   product=p.name
+#   quantity = Article.where(product_id:p.id).count
+#   last_imported = Article.where(product_id: p.id).order(:created_at).last
+#   @stores << { name: product, quantity: quantity, last_imported: last_imported }
+# end
+# @stores.each do |s|
+#   s[:product]
+# end
