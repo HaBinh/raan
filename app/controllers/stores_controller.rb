abd32b02b5f8b8b409e96d4df9f86871c574d677
viethoc <viethocpro@gmail.com>
2017-09-26 15:10:57 +0700

    merge

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,4 @@
 class StoresController < ApplicationController
-<<<<<<< HEAD
       def index
         @articles = Article.all
         @stores = Array.new
@@ -8,16 +7,6 @@ class StoresController < ApplicationController
           @quantity = Article.where(product_id:p.id, status: "t").count
           @store = Article.where(product_id: p.id, status: "t").order(:created_at).last
           # byebug
-=======
-    def index
-      @articles = Article.all
-      @stores = Array.new
-      Product.all.each do |p|
-        @product=p.name
-        @quantity = Article.where(product_id:p.id).count
-        @store = Article.where(product_id: p.id).order(:created_at).last
-        unless @store.nil?
->>>>>>> b36197ac2ee8ca0e8e9a88469498deaa483a7085
           @store.status = @quantity
           @stores << @store              
         end   
