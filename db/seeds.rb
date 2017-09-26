8fa53cfca206124c578d8fdecf79fa7780bbe87f
viethoc <viethocpro@gmail.com>
2017-09-26 16:09:00 +0700

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,116 +23,160 @@ User.create(email: 'user@example.com',
     address: address
     )
 end
+
 product=Product.create!(
-  name: "iPhone X",
-  code: "ASD-1234",
-  category: "128GB-GOLD",
-  default_imported_price: 999,
-  default_sale_price: 1234
-)
-product.articles.create!(
-  status: 'exist',
-  imported_price: 1000)
+    name: "iPhone X",
+    code: "ASD-1234",
+    category: "128GB-GOLD",
+    default_imported_price: 999,
+    default_sale_price: 1234
+  )
+5.times do |n|
+  product.articles.create!(
+    status: 'exist',
+    imported_price: 1000)
+end
 
 product1=Product.create!(
-    name: "iPhone 8",
-    code: "LCD-5634",
-    category: "64GB-GOLD",
-    default_imported_price: 888,
-    default_sale_price: 919
-  )
-product1.articles.create!(
-  status: 'exist',
-  imported_price: 900)
+      name: "iPhone 8",
+      code: "LCD-5634",
+      category: "64GB-GOLD",
+      default_imported_price: 888,
+      default_sale_price: 919
+    )
+6.times do |n|
+  product1.articles.create!(
+    status: 'exist',
+    imported_price: 900)
+end
 
 product2=Product.create!(
-    name: "OPPO F7",
-    code: "OSE-4367",
-    category: "18MP-SELFIE",
-    default_imported_price: 345,
-    default_sale_price: 456
-  )
-product2.articles.create!(
-  status: 'exist',
-  imported_price: 345)
+      name: "OPPO F7",
+      code: "OSE-4367",
+      category: "18MP-SELFIE",
+      default_imported_price: 345,
+      default_sale_price: 456
+    )
+7.times do |n|
+  product2.articles.create!(
+    status: 'exist',
+    imported_price: 345)
+end
 
 product3=Product.create!(
-    name: "Samsung Galaxy Note 8",
-    code: "NOT-9384",
-    category: "64GB-GAME",
-    default_imported_price: 899,
-    default_sale_price: 999
-  )
-product3.articles.create!(
-  status: 'exist',
-  imported_price: 901)
+      name: "Samsung Galaxy Note 8",
+      code: "NOT-9384",
+      category: "64GB-GAME",
+      default_imported_price: 899,
+      default_sale_price: 999
+    )
+8.times do |n|
+  product3.articles.create!(
+    status: 'exist',
+    imported_price: 901)
+end
+
+1.times do |n|
+  product3.articles.create!(
+    status: 'sold',
+    imported_price: 901)
+end
 
 product4=Product.create!(
-    name: "Laptop Apple Macbook Air",
-    code: "MMG-2352",
-    category: "128GB-2015",
-    default_imported_price: 1100,
-    default_sale_price: 1200
-  )
-product4.articles.create!(
-  status: 'exist',
-  imported_price: 1100)
+      name: "Laptop Apple Macbook Air",
+      code: "MMG-2352",
+      category: "128GB-2015",
+      default_imported_price: 1100,
+      default_sale_price: 1200
+    )
+9.times do |n|
+  product4.articles.create!(
+    status: 'exist',
+    imported_price: 1100)
+end
 
 product5=Product.create!(
-    name: "Laptop Apple Macbook Pro",
-    code: "MPX-3182",
-    category: "256GB-2017",
-    default_imported_price: 2009,
-    default_sale_price: 2100
-  )
-product5.articles.create!(
-  status: 'exist',
-  imported_price: 2010)
+      name: "Laptop Apple Macbook Pro",
+      code: "MPX-3182",
+      category: "256GB-2017",
+      default_imported_price: 2009,
+      default_sale_price: 2100
+    )
+10.times do |n|
+  product5.articles.create!(
+    status: 'exist',
+    imported_price: 2010)
+end
+
+2.times do |n|
+  product5.articles.create!(
+    status: 'sold',
+    imported_price: 2010)
+end
 
 product6=Product.create!(
-    name: "iPad Pro",
-    code: "WIF-1056",
-    category: "64GB-2017",
-    default_imported_price: 678,
-    default_sale_price: 699
-  )
-product6.articles.create!(
-  status: 'exist',
-  imported_price: 680)
+      name: "iPad Pro",
+      code: "WIF-1056",
+      category: "64GB-2017",
+      default_imported_price: 678,
+      default_sale_price: 699
+    )
+11.times do |n|
+  product6.articles.create!(
+    status: 'exist',
+    imported_price: 680)
+end
 
 product7=Product.create!(
-    name: "Sony Smartwatch 2",
-    code: "SON-2201",
-    category: "1.6Inch-Android",
-    default_imported_price: 123,
-    default_sale_price: 134
-  )
-product7.articles.create!(
-  status: 'exist',
-  imported_price: 120)
+      name: "Sony Smartwatch 2",
+      code: "SON-2201",
+      category: "1.6Inch-Android",
+      default_imported_price: 123,
+      default_sale_price: 134
+    )
+12.times do |n|
+  product7.articles.create!(
+    status: 'exist',
+    imported_price: 120)
+end
+
+3.times do |n|
+  product7.articles.create!(
+    status: 'sold',
+    imported_price: 120)
+end
 
 product8=Product.create!(
-    name: "USB 3.0",
-    code: "IXP-3032",
-    category: "32GB-USB 3.0",
-    default_imported_price: 90,
-    default_sale_price: 99
-  )
-product8.articles.create!(
-  status: 'exist',
-  imported_price: 90)
+      name: "USB 3.0",
+      code: "IXP-3032",
+      category: "32GB-USB 3.0",
+      default_imported_price: 90,
+      default_sale_price: 99
+    )
+14.times do |n|
+  product8.articles.create!(
+    status: 'exist',
+    imported_price: 90)
+end
 
 product9=Product.create!(
-    name: "Bluetooth AirPods Apple",
-    code: "MME-2450",
-    category: "3.5mm-Bluetooth",
-    default_imported_price: 200,
-    default_sale_price: 222
-  )
-product9.articles.create!(
-  status: 'exist',
-  imported_price: 200)
+      name: "Bluetooth AirPods Apple",
+      code: "MME-2450",
+      category: "3.5mm-Bluetooth",
+      default_imported_price: 200,
+      default_sale_price: 222
+    )
+15.times do |n|
+  product9.articles.create!(
+    status: 'exist',
+    imported_price: 200)
+end
 
+4.times do |n|
+  product9.articles.create!(
+    status: 'sold',
+    imported_price: 200)
+end
 
 rates = [0, 0.02, 0.05, 0.1, 0.2, 0.5]
 
