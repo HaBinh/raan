e7d5fdc78509dc0d7a4541d785d093ed56ec29bf
minhthuan <doanminhthuan274@gmail.com>
2017-11-01 14:54:52 +0700

    override parms sign up in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-  before_save -> { skip_confirmation! }
 end
