8653cea85a06c2dcf740ebd6a38083a3edf13dc7
minhthuan <doanminhthuan274@gmail.com>
2017-11-13 21:00:18 +0700

    add ENV gmail and pwd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
      address:              'smtp.gmail.com',
      port:                 587,
      domain:               'gmail.com',
-     user_name:            'doanminhthuan274@gmail.com',
-     password:             'Binhthuan274',
+     user_name:            ENV['GMAIL_USERNAME'],
+     password:             ENV['GMAIL_PASSWORD'],
      authentication:       'plain',
      enable_starttls_auto: true
   }
