86ffee5a1c7470a9748d752fd755d76687abffd2
minhthuan <doanminhthuan274@gmail.com>
2017-11-14 15:26:05 +0700

    change user and pwd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,8 @@ Rails.application.configure do
      address:              'smtp.gmail.com',
      port:                 587,
      domain:               'gmail.com',
-     user_name:            ENV['GMAIL_USERNAME'],
-     password:             ENV['GMAIL_PASSWORD'],
+     user_name:            'vkh@novahub.vn',
+     password:             'RaanDemo',
      authentication:       'plain',
      enable_starttls_auto: true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
      address:              'smtp.gmail.com',
-     port:                 587,
+     port:                 465,
      domain:               'gmail.com',
-     user_name:            ENV['GMAIL_USERNAME'],
-     password:             ENV['GMAIL_PASSWORD'],
+     user_name:            'vkh@novahub.vn',
+     password:             'RaanDemo',
      authentication:       'plain',
      enable_starttls_auto: true
   }
