548c0869bf663997b64e7f53adb41f2a72348acd
minhthuan <doanminhthuan274@gmail.com>
2017-11-16 19:32:08 +0700

    change default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  host = 'raan-thuan-api.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://raan-thuan-api.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
