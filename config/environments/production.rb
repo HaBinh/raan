17463b9ba3d58ed08f266a83155924e590c7da66
Minh Than <thanqminh@gmail.com>
2017-11-21 15:12:30 +0700

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://raan-thuan-api.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://vkh.novahub.vn/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
      address:              'smtp.gmail.com',
      port:                 587,
-     domain:               'raan-thuan-api.herokuapp.com',
+     domain:               'vkh.novahub.vn',
      user_name:            'vkh@novahub.vn',
      password:             'RaanDemo',
      authentication:       'plain',
