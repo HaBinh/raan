57834ba550535e109fa383bf9c86007b5db253fb
Minh Than <thanqminh@gmail.com>
2017-11-27 14:39:51 +0700

    update deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ namespace :custom do
     on roles(:docker) do |host|
       execute "cp #{deploy_to}/shared/config/database.yml #{deploy_to}/current/config/"
       puts "================Starting Docker setup===================="
-      execute "cd #{deploy_to}/current && bundle exec rake db:migrate && bundle exec pumactl -P tmp/pids/puma.pid stop && bundle exec pumactl -P tmp/pids/puma.pid start"
+      execute "source ~/.bash_profile && gitcd #{deploy_to}/current && bundle exec rake db:migrate && bundle exec pumactl -P tmp/pids/puma.pid stop && bundle exec pumactl -P tmp/pids/puma.pid start"
     end
   end
 end
