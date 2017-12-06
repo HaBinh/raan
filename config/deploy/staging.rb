63cc834e1ed0693a8c0f28ef2cf9f45af4f925fe
Minh Than <thanqminh@gmail.com>
2017-12-06 13:57:20 +0700

    change port to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-server 'raan@raan.novahub.vn', port: 22, roles: %w{deploy}, primary: tru
+server 'raan@raan.novahub.vn', port: 22, roles: %w{deploy}, primary: true
 set :stage,           :staging
 set :port, 3000
