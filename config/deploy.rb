2d951110c7deb3b53b8dbf1b1f391052c4feaaa3
Minh Than <thanqminh@gmail.com>
2017-12-06 10:57:44 +0700

    fix deploy to

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,11 @@ set :application, 'raan'
 set :repo_url, "git@bitbucket.org:novahub/besm.git"
 set :stages, ["staging", "production"]
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "~/apps/#{fetch(:application)}"
 
 namespace :custom do
   task :setup_environment_then_start_server do
-    on roles(:docker) do |host|
+    on roles(:deploy) do |host|
       execute "cp #{deploy_to}/shared/config/database.yml #{deploy_to}/current/config/"
       puts "================Load Ruby on Rails, migrate DB then start server===================="
       execute "source ~/.bash_profile && cd #{deploy_to}/current && bundle exec rake db:migrate && bundle exec pumactl -F config/puma.rb restart"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
-server 'raan.novahub.vn', port: 22, roles: %w{deploy}, primary: true
+server 'raan@raan.novahub.vn', port: 22, roles: %w{deploy}, primary: true
 set :stage,           :staging
