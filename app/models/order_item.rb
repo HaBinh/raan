fc16d69af45b2e9d271626f4a8af7902fda65641
minhthuan <doanminhthuan274@gmail.com>
2018-01-13 09:13:07 +0700

    round up price

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,11 +6,11 @@ class OrderItem < ApplicationRecord
   
   def calculate_amount(price_sale)
     amount = price_sale * quantity.to_i * (1 - discounted_rate)
-    self.update_attributes(amount: amount)
+    self.update_attributes(amount: amount.round(-2))
   end
 
   def return_calculate_after_return(quantity_return)
-    self.amount = (self.amount / self.quantity ) * ( quantity - quantity_return )
+    self.amount = ((self.amount / self.quantity ) * ( quantity - quantity_return )).round(-2)
     self.quantity = self.quantity - quantity_return
     self.save
   end
