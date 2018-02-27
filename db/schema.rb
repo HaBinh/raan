59f9bd4d3b6c3f1248379b7b7242508c42dcdea5
viethoc <viethocpro@gmail.com>
2018-02-27 20:14:43 +0700

    migrate category product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226102715) do
+ActiveRecord::Schema.define(version: 20180227130830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,8 @@ ActiveRecord::Schema.define(version: 20180226102715) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
-    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_categories_on_product_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -104,6 +102,8 @@ ActiveRecord::Schema.define(version: 20180226102715) do
     t.datetime "updated_at", null: false
     t.string "unit"
     t.boolean "active", default: true
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -139,11 +139,11 @@ ActiveRecord::Schema.define(version: 20180226102715) do
 
   add_foreign_key "articles", "order_items"
   add_foreign_key "articles", "products"
-  add_foreign_key "categories", "products"
   add_foreign_key "imports", "products"
   add_foreign_key "imports", "users"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "customers"
   add_foreign_key "product_discounted_rates", "products"
+  add_foreign_key "products", "categories"
 end
