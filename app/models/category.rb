e569ac6e587560d93d827f26ead29d848d2db0c8
minhthuan <doanminhthuan274@gmail.com>
2018-03-09 14:45:09 +0700

    add count category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,13 @@
 class Category < ApplicationRecord
   has_many :products
-    def self.get_pagination(page, per_page, search_text)
-      
-      if page.presence
-        @ketqua = Category.paginate(:page => page, :per_page => per_page)
-      end
-
-      @ketqua = Category.all
-
-		return @ketqua, @total
+  def self.get_pagination(page, per_page, search_text)
+    
+    if page.presence
+      @ketqua = Category.paginate(:page => page, :per_page => per_page)
     end
+
+    @ketqua = Category.all
+    @total = Category.count
+  return @ketqua, @total
+  end
 end
